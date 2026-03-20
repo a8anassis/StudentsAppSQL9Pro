9df29a8b525f6e15313c6a9c014a6d89d0bf298e
a8anassis <a8anassis@gmail.com>
2026-03-20 19:41:26 +0200

    chore: add insert service for student

---
+++ StudentsAppSQL9Pro/Services/StudentServiceImpl.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using StudentsAppSQL9Pro.DAO;
+using StudentsAppSQL9Pro.DTO;
+using StudentsAppSQL9Pro.Models;
+using System.Transactions;
+
+namespace StudentsAppSQL9Pro.Services
+{
+    public class StudentServiceImpl : IStudentService
+    {
+        private readonly IStudentDAO _studentDAO;
+        private readonly IMapper _mapper;
+        private readonly ILogger<StudentServiceImpl> _logger;
+
+        public StudentServiceImpl(IStudentDAO studentDAO, IMapper mapper, ILogger<StudentServiceImpl> logger)
+        {
+            _studentDAO = studentDAO;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public StudentReadOnlyDTO? InsertStudent(StudentReadOnlyDTO studentInsertDTO)
+        {
+            StudentReadOnlyDTO studentReadOnlyDTO;
+
+            try
+            {
+                using TransactionScope scope = new TransactionScope();
+                Student student = _mapper.Map<Student>(studentInsertDTO);
+                Student? insertedStudent = _studentDAO.Insert(student);
+                studentReadOnlyDTO = _mapper.Map<StudentReadOnlyDTO>(student);
+                _logger.LogInformation("Student {Firstname} {Lastname} inserted successfully.", 
+                    studentReadOnlyDTO.Firstname, studentReadOnlyDTO.Lastname);
+                scope.Complete();
+                return studentReadOnlyDTO;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Student insertion failed for {Firstname} {Lastname}",
+                    studentInsertDTO.Firstname, studentInsertDTO.Lastname);
+            }
+        }
+
+        public void DeleteStudent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<StudentReadOnlyDTO> GetAllStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StudentReadOnlyDTO GetStudent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public void UpdateStudent(StudentUpdateDTO studentUpdateDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
